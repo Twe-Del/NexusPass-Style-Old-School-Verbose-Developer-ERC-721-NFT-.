443248e9ff5b0e9d56acd0b5e9c49d2bca23fc34
Twe-Del <twellebr1@outlook.com>
2026-03-06 15:33:41 +0400

    burg

--- contracts/FTE.sol
+++ contracts/FTE.sol
@@ -1,10 +1,17 @@
-
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.4.0
-pragma solidity ^0.8.27;
+pragma solidity 0.8.24; 
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract FTE is ERC20 {
-    constructor() ERC20("FTE", "MTK") {}
+/// @title Quantum Token Contract
+/// @author CorporateDev99
+/// @notice A standard deflationary token with modern syntax and explicit definitions.
+contract QuantumToken is ERC20, ERC20Burnable {
+    
+    uint256 private constant INITIAL_SUPPLY = 7500000;
+
+    constructor() ERC20("QuantumToken", "QNTM") {
+        _mint(msg.sender, INITIAL_SUPPLY * (10 ** decimals()));
+    }
 }
